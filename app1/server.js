0902c5c26fa102073803443f7e7ec2b1a513f4da
workytip <workytip@gmail.com>
2025-11-22 23:30:44 +0200

    test webhook

--- app1/server.js
+++ app1/server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     <html>
       <head><title>App 1</title></head>
       <body style="background-color: #f0f8ff;">
-        <h1>🎯 Application 1</h1>
+        <h1> Application 1</h1>
         <p>Server: ${req.headers.host}</p>
         <p>Time: ${new Date().toLocaleString()}</p>
         <p>This is Load Balanced App 1</p>
